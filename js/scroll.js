e9145953f6f9ad67be1f3d8475d4ae4e4f66ebd1
rodrigo1821 <39421411+rodrigo1821@users.noreply.github.com>
2024-06-24 16:44:55 -0300

    Portfólio

---
+++ js/scroll.js
@@ -0,0 +1,17 @@
+// smooth-scroll.js
+
+$(document).ready(function(){
+    // Rolar suavemente ao clicar em links do menu
+    $('a.nav-link').on('click', function(event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
+
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function(){
+                window.location.hash = hash;
+            });
+        }
+    });
+});
